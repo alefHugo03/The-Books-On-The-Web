735ed014e25530a2e0f1577ad9b62d5bafcf45d8
Alef <alef65507@gmail.com>
2025-11-03 19:05:56 -0300

    fazendo a pesquisa

--- public/src/pesquisa.php
+++ public/src/pesquisa.php
@@ -2,22 +2,35 @@
 /* Começando a pesquisa */
 require_once 'conection/conectionBD.php';
 
-if (!isset($_POST['pesquisa'])) {
+if (!isset($_GET['pesquisa'])) {
     die("Nenhum termo de pesquisa fornecido.");
 };
 
-$pesquisa = $_POST['pesquisa'];
+$pesquisa = $-$_GET['pesquisa'];
 
 $sql = "SELECT * FROM livros WHERE titulo LIKE $pesquisa OR autor LIKE $pesquisa OR genero LIKE $pesquisa OR livro LIKE $pesquisa";
 $result = mysqli_query($conexion, $sql);
 
 
-
-$sql = "SELECT 1 FROM usuarios WHERE email = ? LIMIT 1";
-
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$emailParaVerificar]);
-
-
-
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <base href="http://localhost/ProjetoM2/The-Books-On-The-Web/public/">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="shortcut icon" href="styles/img/favicon.svg"  type="image/x-icon" class="favicon">
+    <title>The Books On The Web</title>
+</head>
+<body>
+    <header id="header-placeholder"></header>
+            
+    <main>
+
+    </main>
+
+    <footer id="footer-placeholder" class="caixa-footer"></footer>
+</body>
+<script src="scripts/script.js"></script>
+</html>
